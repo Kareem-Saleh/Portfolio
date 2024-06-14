ab6a7bc9838357d768fdc9d341bc928c33dd9f46
Kareem Saleh <kl.kasaleh98@gmail.com>
2024-06-14 13:41:33 +0100

    Replaced Blog with Boards + started using Prettier for formatting

--- scripts/theme_change.js
+++ scripts/theme_change.js
@@ -4,20 +4,33 @@ const themeButton = document.querySelector(".theme-button");
 let theme = localStorage.getItem("theme");
 
 if (theme == null) {
-    theme = localStorage.setItem("theme", "light");
-};
+  theme = localStorage.setItem("theme", "light");
+}
 
 if (theme == "light") {
-    root.style.setProperty("--accent-black", "rgb(23, 23, 23)");
-    root.style.setProperty("--accent-yellow", "rgb(235, 235, 42)");
-    root.style.setProperty("--accent-white", "rgb(255,255,255)");
-    root.style.setProperty("--text-white", "rgb(255,255,255)");
-    root.style.setProperty("--text-black", "rgb(0,0,0)");
-    root.style.setProperty("--body-color", "rgb(245, 248, 164)");
-    root.style.setProperty("--body-shadow", "rgba(0, 0, 0, 0.211)");
-};
+  root.style.setProperty("--accent-black", "rgb(23, 23, 23)");
+  root.style.setProperty("--accent-yellow", "rgb(235, 235, 42)");
+  root.style.setProperty("--accent-white", "rgb(255,255,255)");
+  root.style.setProperty("--text-white", "rgb(255,255,255)");
+  root.style.setProperty("--text-black", "rgb(0,0,0)");
+  root.style.setProperty("--body-color", "rgb(245, 248, 164)");
+  root.style.setProperty("--body-shadow", "rgba(0, 0, 0, 0.211)");
+}
 
 if (theme == "dark") {
+  root.style.setProperty("--body-color", "rgb(0,0,0)");
+  root.style.setProperty("--accent-black", "rgb(236, 209, 209)");
+  root.style.setProperty("--accent-yellow", "rgb(52, 48, 48)");
+  root.style.setProperty("--accent-white", "rgb(25, 25, 25)");
+  root.style.setProperty("--text-white", "rgb(2, 2, 2)");
+  root.style.setProperty("--text-black", "rgb(255, 255, 255)");
+  root.style.setProperty("--body-shadow", "rgba(255, 255, 255, 0.145)");
+}
+
+themeButton.addEventListener("click", () => {
+  theme = localStorage.getItem("theme");
+
+  if (theme == "light") {
     root.style.setProperty("--body-color", "rgb(0,0,0)");
     root.style.setProperty("--accent-black", "rgb(236, 209, 209)");
     root.style.setProperty("--accent-yellow", "rgb(52, 48, 48)");
@@ -25,30 +38,17 @@ if (theme == "dark") {
     root.style.setProperty("--text-white", "rgb(2, 2, 2)");
     root.style.setProperty("--text-black", "rgb(255, 255, 255)");
     root.style.setProperty("--body-shadow", "rgba(255, 255, 255, 0.145)");
-};
+    theme = localStorage.setItem("theme", "dark");
+  }
 
-themeButton.addEventListener("click", () => {
-    theme = localStorage.getItem("theme");
-
-    if (theme == "light") {
-        root.style.setProperty("--body-color", "rgb(0,0,0)");
-        root.style.setProperty("--accent-black", "rgb(236, 209, 209)");
-        root.style.setProperty("--accent-yellow", "rgb(52, 48, 48)");
-        root.style.setProperty("--accent-white", "rgb(25, 25, 25)");
-        root.style.setProperty("--text-white", "rgb(2, 2, 2)");
-        root.style.setProperty("--text-black", "rgb(255, 255, 255)");
-        root.style.setProperty("--body-shadow", "rgba(255, 255, 255, 0.145)");
-        theme = localStorage.setItem("theme", "dark");
-    };
-    
-    if (theme == "dark") {
-        root.style.setProperty("--accent-black", "rgb(23, 23, 23)");
-        root.style.setProperty("--accent-yellow", "rgb(235, 235, 42)");
-        root.style.setProperty("--accent-white", "rgb(255,255,255)");
-        root.style.setProperty("--text-white", "rgb(255,255,255)");
-        root.style.setProperty("--text-black", "rgb(0,0,0)");
-        root.style.setProperty("--body-color", "rgb(245, 248, 164)");
-        root.style.setProperty("--body-shadow", "rgba(0, 0, 0, 0.211)");
-        theme = localStorage.setItem("theme", "light");
-    };
+  if (theme == "dark") {
+    root.style.setProperty("--accent-black", "rgb(23, 23, 23)");
+    root.style.setProperty("--accent-yellow", "rgb(235, 235, 42)");
+    root.style.setProperty("--accent-white", "rgb(255,255,255)");
+    root.style.setProperty("--text-white", "rgb(255,255,255)");
+    root.style.setProperty("--text-black", "rgb(0,0,0)");
+    root.style.setProperty("--body-color", "rgb(245, 248, 164)");
+    root.style.setProperty("--body-shadow", "rgba(0, 0, 0, 0.211)");
+    theme = localStorage.setItem("theme", "light");
+  }
 });
