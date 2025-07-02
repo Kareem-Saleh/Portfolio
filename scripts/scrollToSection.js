c9270655b3a714698590149ac7aad38f122eecff
Kareem Saleh <kl.kasaleh98@gmail.com>
2025-07-02 13:38:15 +0100

    Fixed layout, added new sections and added simple navigation to blog posts

--- scripts/scrollToSection.js
+++ scripts/scrollToSection.js
@@ -1,13 +1,19 @@
+const rightSide = document.querySelector(".content-right");
+const computedStyle = window.getComputedStyle(rightSide);
+const paddingTop = computedStyle.getPropertyValue("padding-top").slice(0, -2);
+
 document.querySelectorAll(".jump-link").forEach((link) => {
+  if (link.getAttribute("href") == "../index.html") {
+    return;
+  }
   link.addEventListener("click", (e) => {
     e.preventDefault();
     const targetId = link.getAttribute("href");
+
     const targetSection = document.querySelector(targetId);
 
-    const rightSide = document.querySelector(".content-right");
-
     rightSide.scrollTo({
-      top: targetSection.offsetTop,
+      top: targetSection.offsetTop - paddingTop,
       behavior: "smooth",
     });
   });
