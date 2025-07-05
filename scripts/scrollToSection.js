0fd74590b3f73d14c669604589264a176453767a
Kareem Saleh <kl.kasaleh98@gmail.com>
2025-07-05 12:46:40 +0100

    Written blog sections for Boards, Spreading Law and Portfolio Website

--- scripts/scrollToSection.js
+++ scripts/scrollToSection.js
@@ -1,12 +1,20 @@
 const rightSide = document.querySelector(".content-right");
 const computedStyle = window.getComputedStyle(rightSide);
 const paddingTop = computedStyle.getPropertyValue("padding-top").slice(0, -2);
+const limit = 1152;
 
 document.querySelectorAll(".jump-link").forEach((link) => {
   if (link.getAttribute("href") == "../index.html") {
     return;
   }
+
   link.addEventListener("click", (e) => {
+    const screenWidth = window.innerWidth;
+
+    if (screenWidth < limit) {
+      return;
+    }
+
     e.preventDefault();
     const targetId = link.getAttribute("href");
 
